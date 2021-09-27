5114229ae6536995943d6c679c6a6154e897314b
Alex Kluver <abk98@cornell.edu>
2021-09-27 14:45:18 -0400

    completed lab2ii with comments

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -62,7 +62,7 @@ x = 0
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
-font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 50)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 30)
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
@@ -78,43 +78,71 @@ while True:
     #draw.rectangle((0, 0, width, height), outline=0, fill=0)
  
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
+    
+    #unlocking sequence, A, then B, then A, then both
+    #check for button push on A
     if buttonB.value and not buttonA.value:
         if i == 0 or i == 2:
             if i == 0:
+                #clear screen on first push
                 disp.fill(color565(0,0,0))
-            draw.rectangle((0,0,width,height), outline = 0, fill = 0)
-            disp.fill_rectangle(h,0,35 ,width,color565(0,255,0))
+                draw.rectangle((0,0,width,height),outline = 0, fill = 0)
+            #display green squares to show that correct button has been pushed
+            draw.text((45,h),"A",font = font2, fill = "#FFFFFF")
+            disp.image(image,rotation)
+            disp.fill_rectangle(0,100,h + 35,50,color565(0,255,0))
             i += 1
             h += 35
         else:
-            draw.rectangle((0,0,width,height), outline = 0,fill = 0)
-            disp.fill_rectangle(h,0,35,width,color565(255,0,0))
+            #display red rectangle to show wrong button push
+            disp.fill_rectangle(h,45,35,50,color565(255,0,0))
     if buttonA.value and not buttonB.value:
         if i == 0:
+            #clear screen on first push
             disp.fill(color565(0,0,0))
         if i == 1:
-            draw.rectangle((0,0,width,height), outline=0, fill=0)
-            disp.fill_rectangle(h,0,35,width,color565(0,255,0))
+            #display rectangle and corresponding button push
+            draw.text((45,h),"B",font = font2, fill = "#FFFFFF")
+            disp.image(image,rotation)
+            disp.fill_rectangle(0,100,70,50,color565(0,255,0))
             i += 1
             h += 35
         else:
-            draw.rectangle((0,0,width,height), outline=0, fill=0)
-            disp.fill_rectangle(h,0,35,width,color565(255,0,0))
+            #display a red square to signify wrong button push
+            disp.fill_rectangle(h,45,35,50,color565(255,0,0))
     if not buttonA.value and not buttonB.value and i == 3:
+        #display final button sequence and message to see time
+        draw.text((0,h),"A + B", font = font2, fill = "#FFFFFF")
+        draw.text((160,0), "Push A\n to see\n time", font = font, fill = "#FFFFFF")
+        disp.image(image,rotation)
+        disp.fill_rectangle(0,100,135,50,color565(0,255,0))
+        time.sleep(1)
+        #wait for button push
+        while buttonA.value:
+            time.sleep(0.1)   
+        #clear screen then display time using bars
         disp.fill(color565(0,0,0))
         while True:
+            #infinite while loop to display clock
             draw.rectangle((0,0,width,height),outline=0,fill=0)
             time_hr = int(strftime("%H"))
             time_min = int(strftime("%M"))
             time_sec = int(strftime("%S"))
             y = 45
-            #draw.text((15, y), time_display, font=font2, fill="#FFFFFF")
-            #disp.image(image,rotation)
-            disp.fill_rectangle(0,0,35,time_hr * 10,color565(0,0,255))
-            disp.fill_rectangle(50,0,35,time_min * 4,color565(0,255,255))
-            disp.fill_rectangle(100,0,35,time_sec * 4,color565(255,255,0))
-            time.sleep(1)
+            draw.text((time_hr*9 + 1,0), str(time_hr), font=font2, fill="#FFFFFF")
+            draw.text((time_min* 3,50),str(time_min),font = font2, fill = "#FFFFFF")
+            draw.text((time_sec * 3,100),str(time_sec), font = font2, fill = "#FFFFFF")
+            disp.image(image,rotation)
+            disp.fill_rectangle(0,240 - time_hr * 9,35,240,color565(255,0,255))
+            disp.fill_rectangle(50,240 - time_min * 3,35,240,color565(0,255,255))
+            disp.fill_rectangle(100,240-time_sec * 3,35,240, color565(255,255,0))
+            time.sleep(0.9)
             if time_sec == 0:
-                disp.fill(color565(0,0,0))
+                 disp.fill(color565(0,0,0))
+            #reset back to beginning using button push
+            if not buttonA.value or not buttonB.value:
+                i = 0
+                h = 0
+                break
     # Display image.
     time.sleep(0.1)
